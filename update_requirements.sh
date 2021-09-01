ad7caa32beb93a68fdb89b719fc4e5ae4f07b6c1
Norman Moeschter <norman.moeschter@gmail.com>
2021-09-01 06:32:47 +0100

    Update dependencies.

--- update_requirements.sh
+++ update_requirements.sh
@@ -2,7 +2,10 @@
 
 set -Eeo pipefail
 
+current_date=$(LC_ALL=en_US.utf8 date --utc)
+
 echo -e "\e[32mCompile python dependencies.\e[39m"
 pip-compile --rebuild --upgrade --output-file requirements.txt requirements.in
+sed -i "1i# Compile date: $current_date" requirements.txt
 
 echo -e "\e[32mDone. Check requirements.txt.\e[39m"
